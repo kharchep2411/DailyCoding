a734c402acf2747e1e853cb63dca396891aa72b1
Prasanna <pkharche2411@gmail.com>
2021-12-27 11:09:43 -0500

    file deleted

--- DailyLearning/src/main/java/com/daily/code/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.daily.code.repository;
-
-import com.daily.code.entity.Department;
-import com.daily.code.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long>
-{
-}
